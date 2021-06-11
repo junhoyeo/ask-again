fdf53a77a70d709d4bc699a375e16410c2797785
Junho Yeo <hanaro0704@gmail.com>
2021-06-12 06:10:47 +0900

    Exit with status code based on response

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { prompt } from 'inquirer';
 
 export const main = () => {
-  prompt([{ message: 'Really?', type: 'confirm', name: 'youSaidOkay' }]);
+  prompt([{ message: 'Really?', type: 'confirm', name: 'youSaidOkay' }]).then(
+    ({ youSaidOkay }) => {
+      process.exit(youSaidOkay ? 0 : 1);
+    },
+  );
 };
 
 main();
